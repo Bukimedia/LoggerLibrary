e5600a6c9ae5136093f1f6b38ea180e3f27e4288
Eduardo <eduardo.leon.pavon@gmail.com>
2014-05-24 23:32:26 +0200

    Updated namespace

--- Logger.cs
+++ Logger.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using log4net;
 using log4net.Config;
 
-namespace LoggerLibrary
+namespace Bukimedia.LoggerLibrary
 {
     enum LogLevel { Debug, Info, Warn, Error, Fatal };
 
